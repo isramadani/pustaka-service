cc1c1c723c6899d889fbb9d58eaa7394fe392d53
isramadani <113502867+isramadani@users.noreply.github.com>
2023-01-04 14:48:19 +0700

    Add files via upload

---
+++ src/main/java/com/israa/pustaka/controller/PengembalianController.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.israa.pustaka.controller;
+
+import com.israa.pustaka.FormPengembalian;
+import com.israa.pustaka.model.Pengembalian;
+import com.israa.pustaka.service.PengembalianService;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author IIISI
+ */
+public class PengembalianController {
+    private final PengembalianService pengembalianService;
+    private final FormPengembalian formPengembalian;
+    
+    public PengembalianController(FormPengembalian formPengembalian){
+        this.formPengembalian = formPengembalian;
+        pengembalianService = new PengembalianService();
+    }
+    
+    public void bersihForm(){
+        formPengembalian.getTxtPengembalianId().setText("");
+        formPengembalian.getTxtPeminjamanId().setText("");
+        formPengembalian.getTxtTglDikembalikan().setText("");
+        formPengembalian.getTxtTerlambat().setText("");
+        formPengembalian.getTxtDenda().setText("");
+    }
+    
+    public void getPengembalianId(){
+        Long id = Long.parseLong(formPengembalian.getTxtPengembalianId().getText());
+        Pengembalian pengembalian = pengembalianService.getPengembalian(id);
+        if (pengembalian != null) {
+            formPengembalian.getTxtPeminjamanId().setText(pengembalian.getPeminjamanId().toString());
+            formPengembalian.getTxtTglDikembalikan().setText(pengembalian.getTglDikembalikan());
+            formPengembalian.getTxtTerlambat().setText(Integer.toString(pengembalian.getTerlambat()));
+            formPengembalian.getTxtDenda().setText(Double.toString(pengembalian.getDenda()));
+        } else {
+            JOptionPane.showMessageDialog(formPengembalian, "Data Tidak Ada");
+        }
+    }
+    
+    public void savePengembalian(){
+        Pengembalian pengembalian = new Pengembalian();
+        pengembalian.setPeminjamanId(Long.parseLong(formPengembalian.getTxtPeminjamanId().getText()));
+        pengembalian.setTglDikembalikan(formPengembalian.getTxtTglDikembalikan().getText());
+        pengembalian.setTerlambat(Integer.parseInt(formPengembalian.getTxtTerlambat().getText()));
+        pengembalian.setDenda(Double.parseDouble(formPengembalian.getTxtDenda().getText()));
+        pengembalian = pengembalianService.savePengembalian(pengembalian);
+        if (pengembalian !=null){
+            formPengembalian.getTxtPengembalianId().setText(pengembalian.getPengembalianId().toString());
+            JOptionPane.showMessageDialog(formPengembalian, "Entri Data Berhasil");
+        }else{
+            JOptionPane.showMessageDialog(formPengembalian, "Entri Data Gagal");
+        }
+    }
+    
+    public void updatePengembalian(){
+        Pengembalian pengembalian = new Pengembalian();
+        pengembalian.setPengembalianId(Long.parseLong(formPengembalian.getTxtPeminjamanId().getText()));
+        pengembalian.setPeminjamanId(Long.parseLong(formPengembalian.getTxtPeminjamanId().getText()));
+        pengembalian.setTglDikembalikan(formPengembalian.getTxtTglDikembalikan().getText());
+        pengembalian.setTerlambat(Integer.parseInt(formPengembalian.getTxtTerlambat().getText()));
+        pengembalian.setDenda(Double.parseDouble(formPengembalian.getTxtDenda().getText()));
+        pengembalian = pengembalianService.updatePengembalian(pengembalian);
+        if (pengembalian !=null){
+            formPengembalian.getTxtPengembalianId().setText(pengembalian.getPengembalianId().toString());
+            JOptionPane.showMessageDialog(formPengembalian, "Update Data Berhasil");
+        }else{
+            JOptionPane.showMessageDialog(formPengembalian, "Update Data Gagal");
+        }
+    }
+    
+    public void deletePengembalian(){
+        Long id = Long.parseLong(formPengembalian.getTxtPengembalianId().getText());
+        pengembalianService.deletePengembalian(id);
+        JOptionPane.showMessageDialog(formPengembalian, "Delete Data Berhasil");
+    }
+    
+    public void viewTabel(){
+        DefaultTableModel tabelModel = (DefaultTableModel) formPengembalian.getTabelPengembalian().getModel();
+        tabelModel.setRowCount(0);
+        List<Pengembalian> pengembalianList = pengembalianService.getAllPengembalian();
+        for(Pengembalian pengembalian : pengembalianList){
+            Object[]row = {
+                pengembalian.getPengembalianId(),
+                pengembalian.getPeminjamanId(),
+                pengembalian.getTglDikembalikan(),
+                pengembalian.getTerlambat(),
+                pengembalian.getDenda()
+            };
+            tabelModel.addRow(row);
+        }
+    }
+}
---
+++ src/test/java/com/israa/pustaka/PustakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israa.pustaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PustakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
